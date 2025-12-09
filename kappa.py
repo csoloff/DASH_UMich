965a1367795a892f2f8bcba6948dea56d735ca25
csoloff <cassidysoloff@icloud.com>
2025-12-09 15:49:25 -0700

    Fixed typo

--- kappa.py
+++ kappa.py
@@ -48,7 +48,7 @@ d = simple_read('./inputs/ARCSIX-DASH_P3B_20240610_R1.ict')
 d['kappa'] = np.nan
 for i in range(0,len(d)):
     row = d.loc[i]
-    if np.isfinite(d.loc[i, 'GF']) & np.isfinite(d.loc[i, 'GF']):
+    if np.isfinite(d.loc[i, 'GF']) & np.isfinite(d.loc[i, 'RH']):
         d.loc[i, 'kappa'] = get_root(row.Dp, row.GF, row.RH/100)
 
 d.to_csv('./outputs/RF08_kappa.csv', index=False)
